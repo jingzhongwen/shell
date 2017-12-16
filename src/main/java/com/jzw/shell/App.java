51a190276da5e6fb442feeed5e0403e2286d57ec
jingzhongwen <jingzhongwen@baidu.com>
2017-12-16 21:30:45 +0800

    fix

--- src/main/java/com/jzw/shell/App.java
+++ src/main/java/com/jzw/shell/App.java
@@ -1,18 +1,30 @@
 package com.jzw.shell;
 
+import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 
 /**
  * Created by jingzhongwen on 2017/12/16.
  */
 public class App {
     public static void main(String[] args) throws ParseException {
+        // 创建 Options 对象
         Options options = new Options();
-        CommandLineParser commandLineParser = new DefaultParser();
-        commandLineParser.parse(options,args);
+
+        // 添加 -h 参数
+        options.addOption("h", false, "Lists short help");
+
+        // 添加 -t 参数
+        options.addOption("t", true, "Sets the HTTP communication protocol for CIM connection");
+
+        CommandLineParser commandLineParser = new PosixParser();
+        final CommandLine cmd = commandLineParser.parse(options, args);
+        if (cmd.hasOption("t")) {
+            System.out.println("this is a simple help code.hello " + cmd.getOptionValue("t"));
+        }
         System.out.println("hello");
     }
 }
